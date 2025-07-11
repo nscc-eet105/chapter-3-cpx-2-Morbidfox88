56989858cdc0ed793fb864835b8ff0a9d053ed1f
Chad Collard <chadcollard1567@gmail.com>
2025-07-11 10:58:48 -0400

    amend chap 3 cpx 2

--- code.py
+++ code.py
@@ -1,21 +1,21 @@
 from adafruit_circuitplayground import cp
 import time# Write your code here :-)
 
-BLACK = (0, 0, 0,)
-REST = (.1)
-
 while True:
     for i in range(10):
         cp.pixels[i] = (0, 100, 0)
-        time.sleep(REST)
-        cp.pixels[i] = (BLACK)
+        time.sleep(.5)
+
+
+    cp.pixels.fill((0, 0, 0))
+    time.sleep(1.0)
 
 
     for i in range(9, -1, -1):
         cp.pixels[i] = (255, 0, 0)
-        time.sleep(REST)
-        cp.pixels[i] = (BLACK)
+        time.sleep(0.5)
 
-
+    cp.pixels.fill((0, 0, 0))
+    time.sleep(1.0)
 
 
